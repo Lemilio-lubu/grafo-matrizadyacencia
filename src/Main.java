ca3d7a4c77576e9dbc80b22587eddc6bdf5485c8
lemi <lemi@lemi-HP-Pavilion-Laptop-15-cw1xxx.(none)>
2024-01-03 17:42:25 -0500

    uso de matriz de adyacencia

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+
+        MatrizAdyacencia matriz_1 = new MatrizAdyacencia(7);
+
+        matriz_1.crear_vertice("A");
+        matriz_1.crear_vertice("B");
+        matriz_1.crear_vertice("C");
+        matriz_1.crear_vertice("D");
+        matriz_1.crear_vertice("T");
+        matriz_1.crear_vertice("H");
+        matriz_1.crear_vertice("R");
+
+        try {
+            matriz_1.crear_arco("D","B");
+            matriz_1.crear_arco("D","C");
+            matriz_1.crear_arco("B","H");
+            matriz_1.crear_arco("C","R");
+            matriz_1.crear_arco("R","H");
+            matriz_1.crear_arco("H","T");
+            matriz_1.crear_arco("D","B");
+            matriz_1.crear_arco("H","A");
+            matriz_1.crear_arco("H","D");
+
+            matriz_1.imprimir_grafo();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ src/Vertice.java
@@ -0,0 +1,40 @@
+public class Vertice {
+    private String informacion_vertice;
+    private int numero_vertice;
+    public Vertice(String informacion_vertice){
+        this.informacion_vertice = informacion_vertice;
+        this.numero_vertice = -1;
+    }
+
+    /*
+        Agrega el número de vértice para establecer un orden
+     */
+    public void asignarNumeroVertice(int numero_vertice) {
+        this.numero_vertice = numero_vertice;
+    }
+
+    /*
+        Retorna el valor de la información del vértice
+     */
+    public String getInformacionVertice() {
+        return this.informacion_vertice;
+    }
+
+    /*
+        Retorna la comparación de si 2 vertices son iguales
+     */
+    public boolean esIgualVertice(Vertice vertice_1) {
+        return this.informacion_vertice.equals(vertice_1.getInformacionVertice());
+    }
+
+    /*
+        Se da la información de la clase
+     */
+    @Override
+    public String toString() {
+        return "Vertice{" +
+                "informacion_vertice='" + informacion_vertice + '\'' +
+                ", numero_vertice=" + numero_vertice +
+                '}';
+    }
+}
